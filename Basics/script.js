b742c5b9eaee426958b587acf2029af719692662
Manish Jha <manishjha962@gmail.com>
2024-08-24 09:24:03 -0700

    timeline

--- Basics/script.js
+++ Basics/script.js
@@ -1,8 +1,20 @@
-gsap.from('div', {
-    y: 12,
-    opacity: 0,
-    delay: 1,
-    duration: .5,
-    stagger: 0.5
-})
 
+const tl = gsap.timeline()
+
+tl.to('#box', {
+    x: 400,
+    duration: 1,
+    rotate: 360,
+    delay: .5
+})
+tl.to('#box2', {
+    x: 400,
+    scale: 1.4,
+    duration: 1,
+})
+tl.to('#box3', {
+    x: 400,
+    scale: .8,
+    borderRadius: '50%',
+    duration: 1,
+})
